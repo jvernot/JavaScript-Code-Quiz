6d9674a396f117b86ed786520ec055b104676e91
Jason Vernot <jason.vernot@gmail.com>
2020-03-16 22:35:53 -0400

    refactored header, created highscores.html, styles header

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 
 $(document).ready(function() {
 
-    console.log("test")
+    var questions = [
+        {
+            title: "What is the correct way to write an array in JavaScript?",
+            choices: ["var animals = 'dog', 'cat', 'turtle'", "var animals = 1 = (dog), 2 = (cat) 3 = (turtle)", "var animals = (1:dog, 2:cat, 3:turtle)", "var animals = ['dog', 'cat', 'turtle']"],
+            answer: "var animals = ['dog', 'cat', 'turtle']"
+        },
+        {
+            title: "A JavaScript function is enclosed within ______.",
+            choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+            answer: "curly brackets"
+        },
+        {
+            title: "Which opertor is used to assign a value to a variable?",
+            choices: ["-", "*", "=", "/"],
+            answer: "="
+        },
+        {
+            title: "Which of the following demonstrates the proper syntax of an IF statement in JavaScript",
+            choices: ["if x = 5", "if x = 5 then", "if (x ===5)", "if x =5 then"],
+            answer: "if (x ===5)"
+        },
+        {
+            title: "Which is the proper syntax of the start of a for loop",
+            choices: ["for i = 1 to 10", "for(i = 10; i > 10)", "for (i <= 10; i++)", "for (i = 0; i < 10; i++)"],
+            answer: "for (i = 0; i < 10; i++)"
+        },
+        {
+            title: "Is JavaScript case-sensitive?",
+            choices: ["Yes", "No", "Only on the weekends", "Never"],
+            answer: "Yes"
+        },
+    ];
 
     var secondsLeft = 75;
 
@@ -38,38 +69,6 @@ $(document).ready(function() {
     })
 
 
-    var questions = [
-        {
-            title: "What is the correct way to write an array in JavaScript?",
-            choices: ["var animals = 'dog', 'cat', 'turtle'", "var animals = 1 = (dog), 2 = (cat) 3 = (turtle)", "var animals = (1:dog, 2:cat, 3:turtle)", "var animals = ['dog', 'cat', 'turtle']"],
-            answer: "var animals = ['dog', 'cat', 'turtle']"
-        },
-        {
-            title: "A JavaScript function is enclosed within ______.",
-            choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-            answer: "curly brackets"
-        },
-        {
-            title: "Which opertor is used to assign a value to a variable?",
-            choices: ["-", "*", "=", "/"],
-            answer: "="
-        },
-        {
-            title: "Which of the following demonstrates the proper syntax of an IF statement in JavaScript",
-            choices: ["if x = 5", "if x = 5 then", "if (x ===5)", "if x =5 then"],
-            answer: "if (x ===5)"
-        },
-        {
-            title: "Which is the proper syntax of the start of a for loop",
-            choices: ["for i = 1 to 10", "for(i = 10; i > 10)", "for (i <= 10; i++)", "for (i = 0; i < 10; i++)"],
-            answer: "for (i = 0; i < 10; i++)"
-        },
-        {
-            title: "Is JavaScript case-sensitive?",
-            choices: ["Yes", "No", "Only on the weekends", "Never"],
-            answer: "Yes"
-        },
-    ];
 
 
 
